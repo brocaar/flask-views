8811f59d660b9270806a3714cbdac2a58423fc76
Orne Brocaar <info@brocaar.com>
2012-02-14 21:20:42 +0000

    Functional test added for UpdateView.

--- flask_views/tests/functional/db/mongoengine/test_edit.py
+++ flask_views/tests/functional/db/mongoengine/test_edit.py
@@ -1,6 +1,6 @@
 from flask import url_for
 
-from flask_views.db.mongoengine.edit import CreateView
+from flask_views.db.mongoengine.edit import CreateView, UpdateView
 from flask_views.tests.functional.db.mongoengine.base import BaseMongoTestCase
 
 
@@ -61,3 +61,50 @@ class CreateViewTestCase(BaseMongoTestCase):
         self.assertEqual(1, self.TestDocument.objects.count())
 
         self.TestDocument.objects.get(username='foo', name='bar')
+
+
+class UpdateViewTestCase(BaseMongoTestCase):
+    """
+    Tests for :py:class:`.UpdateView`.
+    """
+    def setUp(self):
+        super(UpdateViewTestCase, self).setUp()
+
+        class TestView(UpdateView):
+            model = self.TestDocument
+            form_class = self.TestForm
+            template_name = 'model_form_view.html'
+            success_url = 'http://google.com/'
+            get_fields = {
+                'username': 'user',
+            }
+
+        self.app.add_url_rule('/<user>/', view_func=TestView.as_view('test'))
+
+        test_obj = self.TestDocument(username='foo', name='bar')
+        test_obj.save()
+
+    def test_get(self):
+        """
+        Test GET request.
+        """
+        with self.app.test_request_context():
+            response = self.client.get(url_for('test', user='foo'))
+        self.assertEqual(200, response.status_code)
+        self.assertIn('value="foo"', response.data)
+        self.assertIn('value="bar"', response.data)
+
+    def test_post(self):
+        """
+        Test POST request with valid data.
+        """
+        with self.app.test_request_context():
+            response = self.client.post(url_for('test', user='foo'), data={
+                'username': 'john',
+                'name': 'Foo Bar',
+            })
+        self.assertEqual(302, response.status_code)
+        self.assertEqual('http://google.com/', response.headers['Location'])
+        self.assertEqual(1, self.TestDocument.objects.count())
+
+        self.TestDocument.objects.get(username='john', name='Foo Bar')
