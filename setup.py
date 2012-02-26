1cb8be87d16dc52e283f9b4245cc02ec95580f63
Orne Brocaar <info@brocaar.com>
2012-02-26 16:48:45 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Views',
-    version='0.2.1',
+    version='0.3dev',
     url='http://github.com/brocaar/flask-views',
     license='BSD',
     author='Orne Brocaar',
